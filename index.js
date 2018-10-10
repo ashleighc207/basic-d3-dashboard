ef95e1bd4ac2baec07d453fba4581b038d793556
Ashleigh Christopher <ashleighc207@gmail.com>
2018-10-10 15:00:44 -0400

    Fix line chart issue

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-// let data = [75, 42, 80, 54];
+let data = [75, 42, 80, 54];
 
-// let width = 450, height = 200;
+let width = 450, height = 200;
 
-// createBarChart = (data, width, height) => {
-//     let svg = d3.select("main")
-//         .append("svg")
-//         .attr("width", width)
-//         .attr("height", height)
-//         .attr("class", "bar-chart");
+createBarChart = (data, width, height) => {
+    let svg = d3.select(".bar-chart-card")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "bar-chart");
 
-//     let yScale = d3.scaleLinear()
-//         .domain([d3.min(data), d3.max(data)])
-//         .range([height, 0]);
+    let yScale = d3.scaleLinear()
+        .domain([d3.min(data), d3.max(data)])
+        .range([height, 0]);
 
-//     let xScale = d3.scaleOrdinal()
-//         .domain(["","mobile", "tablet", "laptop", "desktop",""])
-//         .range([0, 85, 185, 285, 385, 450]);
+    let xScale = d3.scaleOrdinal()
+        .domain(["","mobile", "tablet", "laptop", "desktop",""])
+        .range([0, 85, 185, 285, 385, 450]);
 
-//     let y_axis = d3.axisLeft()
-//         .ticks(10)
-//         .scale(yScale);
+    let y_axis = d3.axisLeft()
+        .ticks(10)
+        .scale(yScale);
 
-//     let x_axis = d3.axisBottom()
-//         .scale(xScale);
+    let x_axis = d3.axisBottom()
+        .scale(xScale);
 
-//     svg.append("g")
-//         .call(y_axis);
+    svg.append("g")
+        .call(y_axis);
 
-//     svg.append("g")
-//         .call(x_axis)
-//         .attr("transform", "translate(0, 200)");
+    svg.append("g")
+        .call(x_axis)
+        .attr("transform", "translate(0, 200)");
 
 
-//     let barWidth = (400 / data.length);
-//     let barChart = svg.selectAll("rect")
-//         .data(data)
-//         .enter()
-//         .append("rect")
-//         .attr("y", function(d) {
-//             return 200 - d;
-//         })
-//         .attr("height", function(d) {
-//             return d;
-//         })
-//         .attr("width", barWidth - 10)
-//         .attr("transform", function (d, i) {
-//              let translate = [barWidth * i + 40, 0];
-//              return "translate("+ translate +")";
-//         })
-//     .attr("class", "bar");
-// }
+    let barWidth = (400 / data.length);
+    let barChart = svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("y", function(d) {
+            return 200 - d;
+        })
+        .attr("height", function(d) {
+            return d;
+        })
+        .attr("width", barWidth - 10)
+        .attr("transform", function (d, i) {
+             let translate = [barWidth * i + 40, 0];
+             return "translate("+ translate +")";
+        })
+    .attr("class", "bar");
+}
 
-// createBarChart(data, width, height)
+createBarChart(data, width, height)
 
 createLineChart = () => {
     let lineData = [
@@ -87,7 +87,7 @@ createLineChart = () => {
 
     // set the scales
     let yScale = d3.scaleLinear()
-        .domain([0, 100])
+        .domain([200, 600])
         .range([height, 0]);
 
     let xScale = d3.scaleOrdinal()
@@ -103,19 +103,22 @@ createLineChart = () => {
         .append("svg")
         .attr("width", width)
         .attr("height", height)
-        .attr("class", "bar-chart")
+        .attr("class", "line-chart")
 
     lineChart.append("path")
         .data([lineData])
         .attr("d", line)
+        .attr("fill", "none")
+        .attr("stroke", "rebeccapurple")
+        .attr("stroke-width", 2)
 
     lineChart.append("g")
-        .attr("transform", "translate(10," + height + ")")
+        .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(xScale))
 
     lineChart.append("g")
-        .attr("transform", "translate(10,0)")
-        .call(d3.axisLeft(yScale))
+        .attr("transform", "translate(0,0)")
+        .call(d3.axisLeft(yScale).ticks(5))
 }
 
 createLineChart()
