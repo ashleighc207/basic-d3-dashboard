36f2791f26869614146ebdadd6774b734c68f016
Ashleigh Christopher <ashleighc207@gmail.com>
2018-10-11 08:46:02 -0400

    Change styling off donut chart

--- index.js
+++ index.js
@@ -135,43 +135,41 @@ createLineChart()
 
 
 createDonutChart = () => {
-    let donutData = [
-                  {
-                    "age": "0-15",
-                    "percentage": 0.01233,
-                    "previous": 0.00124
-                  },
-                  {
-                    "age": "16-30",
-                    "percentage": 0.15695,
-                    "previous": 0.02678 
-                  },
-                  {
-                    "age": "31-45",
-                    "percentage": 0.40673,
-                    "previous": 0.05688
-                  },
-                  {
-                    "age": "46-60",
-                    "percentage": 0.33909,
-                    "previous": 0.40673
-                  },
-                  {
-                    "age": "61-75",
-                    "percentage": 0.05688,
-                    "previous": 0.33909
-                  },
-                  {
-                    "age": "76-90",
-                    "percentage": 0.02678,
-                    "previous": 0.15695
-                  },
-                  {
-                    "age": "91-105",
-                    "percentage": 0.00124,
-                    "previous": 0.01233
-                  }
-    ]
+    let donutData = [{
+        "age": "0-15",
+        "percentage": 0.01233,
+        "previous": 0.00124
+      },
+      {
+        "age": "16-30",
+        "percentage": 0.15695,
+        "previous": 0.02678 
+      },
+      {
+        "age": "31-45",
+        "percentage": 0.40673,
+        "previous": 0.05688
+      },
+      {
+        "age": "46-60",
+        "percentage": 0.33909,
+        "previous": 0.40673
+      },
+      {
+        "age": "61-75",
+        "percentage": 0.05688,
+        "previous": 0.33909
+      },
+      {
+        "age": "76-90",
+        "percentage": 0.02678,
+        "previous": 0.15695
+      },
+      {
+        "age": "91-105",
+        "percentage": 0.00124,
+        "previous": 0.01233
+      }];
 
     let width = 450;
     let height = 200;
@@ -182,9 +180,14 @@ createDonutChart = () => {
         .append("svg")
         .attr("width", width)
         .attr("height", height)
+        .attr("class", "donut-chart-container")
         
     let group = donutChart.append("g")
         .attr("transform", "translate(" + (width/2) + "," + (height/2) + ")");
+
+    let color = d3.scaleOrdinal()
+        .domain(["0-15", "16-30", "31-45","46-60", "61-75", "76-90", "91-105"])
+        .range(["#770087" , "#2b009e" , "#0045b3", "#00c7c1", "#00d756", "#fdfdbb"]);  
 
     let arc = d3.arc()
         .outerRadius(radius)
@@ -199,7 +202,10 @@ createDonutChart = () => {
         .enter()
         .append("path")
         .attr("d", arc)
-        .attr("fill", "rebeccapurple")    
+        .attr('fill', function(d, i) { 
+            return color(d.data.age);
+          }); 
+
 
 }
 
